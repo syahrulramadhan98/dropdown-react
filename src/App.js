fd814a0b792f85574072113aa9d43f4f8cbf76b7
syahrul ramadhan <107018219+syahrulramadhan98@users.noreply.github.com>
2022-06-30 13:52:17 +0700

    dropdown-react with react-select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [datas, setDatas] = useState([])
+  const [userSelect,setUserSelect] = useState("")
+  const [isShow,setIsShow] =useState(false)
+
+  const getData = async() =>{
+    const callData = await fetch("https://pokeapi.co/api/v2/berry/")
+    const valueData = await callData.json()
+    const result = valueData.results.map(data=>{
+      return {
+        label: data.name,
+        value: data.name
+      }
+    })
+    setDatas(result.sort((a,b)=> a.label.localeCompare(b.label)))
+  }
+  useEffect(()=>{
+    getData()
+  }, [])
+
+  const handleSubmit = () =>{
+    setIsShow(state => !state)
+  }
+
+  const handleChange = (value) =>{
+    setUserSelect(value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{isShow ? userSelect : ""}</p>
+      <button onClick={() => handleSubmit()} disabled={!userSelect}>{isShow ? "Hide Button" : "Show Button"}</button>
+      <Select options={datas} onChange={(e)=>handleChange(e.value)}></Select>
     </div>
   );
 }
